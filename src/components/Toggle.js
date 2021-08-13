f4a836854abae38d056b944203420c6d5a72a2d1
Connor Cowan <ccowan003@comcast.net>
2021-08-13 13:05:22 -0700

    init

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  //To create a state variable in our component, we need to call useState and provide an initial value:
+  
+  const [isOn, setIsOn] = useState(false);
+  //conditional rendering
+
+  //cb function / update state / call the setter
+  function handleClick() {
+    setIsOn((setIsOn) => !isOn);
+  }
+  const color = isOn ? "red" : "white";
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}</button>
+  );
 }
 
 export default Toggle;
